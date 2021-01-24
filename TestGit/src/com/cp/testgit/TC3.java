551e1a4f3cdaecc83d6bc055f9739801dd504cef
Saurabh <saurabhole1000@gmail.com>
2021-01-24 20:52:55 +0530

    Commit 3: added TC3 and modifed TestClass

---
+++ TestGit/src/com/cp/testgit/TC3.java
@@ -0,0 +1,8 @@
+package com.cp.testgit;
+
+public class TC3 {
+	int i=0;
+	public void show() {
+		System.out.println("This is TC3 : "+i);
+	}
+}
--- TestGit/src/com/cp/testgit/TestClass.java
+++ TestGit/src/com/cp/testgit/TestClass.java
@@ -4,8 +4,10 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		TestClass2 t= new TestClass2();
+		TC3 tt = new TC3();
 		System.out.println("Hello World, Saurabh");
 		t.show();
+		tt.show();
 	}
 
 }
