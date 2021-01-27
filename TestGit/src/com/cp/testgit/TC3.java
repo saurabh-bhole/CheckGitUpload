a8d569fe2b848a86adc16040e0456c551be85f7a
saurabh-bhole <66585668+saurabh-bhole@users.noreply.github.com>
2021-01-27 07:41:41 +0530

    Update TC3.java

--- TestGit/src/com/cp/testgit/TC3.java
+++ TestGit/src/com/cp/testgit/TC3.java
@@ -3,6 +3,6 @@ package com.cp.testgit;
 public class TC3 {
 	int i=0;
 	public void show() {
-		System.out.println("This is TC3 : "+i);
+		System.out.println("This line is changed to test conflict: "+i);
 	}
 }
