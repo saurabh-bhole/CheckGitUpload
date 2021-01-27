162778bf72d334b489a439652b3a0d40c3a32c5b
Saurabh <saurabhole1000@gmail.com>
2021-01-27 07:45:01 +0530

    Local commit 1

--- TestGit/src/com/cp/testgit/TC3.java
+++ TestGit/src/com/cp/testgit/TC3.java
@@ -4,5 +4,8 @@ public class TC3 {
 	int i=0;
 	public void show() {
 		System.out.println("This line is changed to test conflict: "+i);
+
+		System.out.println("This line is to test conflict in git merge : "+i);
+
 	}
 }
